218f34f32f0e85ccbb4ff1c82aedf843c5e06baf
Vinicius Santiago <vinicius_quartz@hotmail.com>
2022-07-19 11:29:07 -0300

    update script

--- prepare-environments.sh
+++ prepare-environments.sh
@@ -1,11 +1,13 @@
-#!bin/bash
-#Vinicius Santiago
-#prepare-environments
+# !/bin/bash
+# Vinicius Santiago
+# prepare-environments
 
 #install docker
+echo "================== install docker ===================";
 curl -fsSL https://get.docker.com | bash
 
 #install kind
+echo "================== install kind ===================";
 https://kind.sigs.k8s.io/docs/user/quick-start/#installing-from-release-binaries
 curl -Lo ./kind https://kind.sigs.k8s.io/dl/v0.13.0/kind-linux-amd64
 chmod +x ./kind
@@ -13,6 +15,7 @@ mv kind /usr/local/bin
 
 kind create cluster
 
+echo "================== install kubectl ===================";
 #install kubectl
 curl -fsSLo /usr/share/keyrings/kubernetes-archive-keyring.gpg https://packages.cloud.google.com/apt/doc/apt-key.gpg
 
